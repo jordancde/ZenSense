2f63715383f8e459e4e558174a0014c142a77e73
Jordan Dearsley <jordandearsley67@gmail.com>
2016-07-28 09:56:08 -0300

    Initial ZenSense Commit

--- ZenSense/src/zensense/ZenSense.java
+++ ZenSense/src/zensense/ZenSense.java
@@ -9,4 +9,11 @@ public class ZenSense {
         
     }
     
+    public static double[] pullData(int numSensors){
+        double[] ethaneData = new double[numSensors];
+        //BLAHBLAHBLAH;
+        //PULLING DATA FROM TRANSMISSION;
+        //ethaneData = NUMBERSANDSHIT;
+        return ethaneData;
+    }
 }
